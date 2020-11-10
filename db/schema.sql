ce429570986a2688ac79b2e4c6e2cae47dc3222f
Oscar Grajeda <oscargrajeda2@gmail.com>
2020-11-09 23:49:48 -0700

    Wrote SQL queries in schema file

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+id INT AUTO_INCREMENT NOT NULL,
+burger_name VARCHAR(40),
+devoured BOOLEAN NOT NULL DEFAULT FALSE,
+PRIMARY KEY (id)
+);
