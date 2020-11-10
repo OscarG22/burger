bbb4d152f40fb8b522c4ec2bdc2acf7aadb89f31
Oscar Grajeda <oscargrajeda2@gmail.com>
2020-11-10 00:02:09 -0700

    Wrote insert queries to populate the burgers table

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (name_of_burger)
+VALUES ("Hamburger");
+
+INSERT INTO burgers (name_of_burger)
+VALUES ("Cheese Burger");
+
+INSERT INTO burgers (name_of_burger)
+VALUES ("Bacon Cheese Burger");
+
+INSERT INTO burgers (name_of_burger)
+VALUES ("Double Bacon Double Cheese Burger Deluxe");
