6b98fce014687cf8d35a84afac77cb9397e579e6
Oscar Grajeda <oscargrajeda2@gmail.com>
2020-11-11 23:25:04 -0700

    Created the methods that will execute the necessary MySQL commands in the controllers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,14 +4,41 @@ const router = express.Router();
 
 const burger = require("../models/burger.js");
 
+router.get("/", function (req, res) {
+  burger.selectAll(function (data) {
+    const hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
 
-router.get('/', function (req, res) {
-    res.send('');
-  })
-  
-  
-  router.post('/about', function (req, res) {
-    res.send('');
-  })
-  
-  module.exports = router;
+router.post("/api/burgers", function (req, res) {
+  burger.insertOne([
+    "burger_name"
+  ],
+    [
+      req.body.name, req.body.sleepy
+    ], function (result) {
+      res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+  const condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  cat.updateOne({
+    devoured: true
+  }, condition, function (result) {
+    if (result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+module.exports = router;
